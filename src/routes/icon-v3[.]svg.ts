da21f12ebe91fd50029e94b910177c984d91a407
lelouchsage96-cyber <lelouch.sage96@gmail.com>
2026-09-07 19:28:17 +0545

    Add scalable PWA icon

---
+++ src/routes/icon-v3[.]svg.ts
@@ -0,0 +1,10 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { appIconSvgResponse } from "@/lib/app-icon/runtime";
+
+export const Route = createFileRoute("/icon-v3.svg")({
+  server: {
+    handlers: {
+      GET: async () => appIconSvgResponse(false),
+    },
+  },
+});
