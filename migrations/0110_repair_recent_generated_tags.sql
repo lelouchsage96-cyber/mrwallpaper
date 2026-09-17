e32d65525776ad3f9af7b467450d498d8674fc90
lelouchsage96-cyber <lelouch.sage96@gmail.com>
2026-09-17 23:44:47 +0545

    Repair existing generated tag phrases

---
+++ migrations/0110_repair_recent_generated_tags.sql
@@ -0,0 +1,6 @@
+update tags set name = 'black background wallpaper' where name = 'black background wallpap';
+update tags set name = 'dragon illustration wallpaper' where name = 'dragon illustration wall';
+update tags set name = 'motivational quote wallpaper' where name = 'motivational quote wallp';
+update tags set name = 'red dragon phone background' where name = 'red dragon phone backgro';
+update tags set name = 'vertical dragon wallpaper' where name = 'vertical dragon wallpape';
+
